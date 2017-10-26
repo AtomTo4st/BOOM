475f57a260b8ee60f202277b41df0de89b998566
discochance <discochance@users.noreply.github.com>
2017-10-26 13:00:36 +0200

    working

--- ext/tank.py
+++ ext/tank.py
@@ -1,18 +1,20 @@
 import pygame
 import sys
+from collections import namedtuple
 sys.path.append("../")
 
 colors = ["red", "green", "blue", "white"]
 Position = namedtuple("Position", ["x","y"])
 
 class Tank:
-    def __init__(self, _id=0, pos=(0,0), rot=0, aim=0, speed=6, turnspeed=5):
+    def __init__(self, _id=0, pos=Position(0, 0), rot=0, aim=0, speed=6, turnspeed=5):
         self.id = _id
-        self.pos = Position(pos[0], pos[1])
+        self.pos = pos
         self.rot = rot
         self.aim = aim
         self.speed = speed
         self.turnspeed = turnspeed
+        self.setup()
         
     def setup(self):
         self.body = pygame.image.load("/assets/pictures/tank_{}_bottom.png".format(colors[self.id]))
@@ -25,7 +27,7 @@ class Tank:
         return rotated, rect.center
     
     def draw(self, screen, pos, rot, aim=0):
-        self.pos = Position(pos[0], pos[1])
+        self.pos = pos
         self.rot = rot
         self.aim = aim
         screen.blit(self.assignRot(self.body, self.rot))
