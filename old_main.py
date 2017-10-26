26bcbf2dec1452c75392da32b5b4ffc156434264
jontebeh <33085219+jontebeh@users.noreply.github.com>
2017-10-26 10:49:25 +0200

    new old main

--- old_main.py
+++ old_main.py
@@ -6,7 +6,6 @@ import colors
 import os
 from pygame import image
 
-#imports
 
 pygame.init()
 
@@ -15,7 +14,7 @@ screen_w = 800
 screen_h = 800
 
 player_1_pos = (screen_w / 4, screen_h / 4)
-player_2_pos = (800 - (screen_w / 4), 800 - (screen_h / 4))
+player_2_pos = (800 - (screen_w / 4), (screen_h / 4))
 
 
 player_1_speed = 6
@@ -54,7 +53,9 @@ def draw_shoot(list, img_shoot):
     shoot_counter = 0
     for each_shoot in list:
         each_shoot_pos = new_pos(each_shoot[0],each_shoot[1],each_shoot[2],each_shoot[3])
-        screen.blit(img_shoot, each_shoot_pos)
+        screen.blit(img_shoot, (each_shoot_pos[0]-32, each_shoot_pos[1]-32))
+        list[shoot_counter] = ((each_shoot_pos[0], each_shoot_pos[1], each_shoot[2], each_shoot[3]))
+        
         if each_shoot_pos[0] < 0 or each_shoot_pos[0] > screen_w or each_shoot_pos[1] < 0 or each_shoot_pos[1] > screen_h:
             del list[shoot_counter]
         
@@ -70,8 +71,8 @@ img_player_1_gun = pygame.image.load("assets/pictures/tank_red_top.png")
 img_player_2_tank = pygame.image.load("assets/pictures/tank_green_bottom.png")
 img_player_2_gun = pygame.image.load("assets/pictures/tank_green_top.png")
 
-#img_player_1_shoot = 
-#img_player_2_shoot = 
+img_player_1_shoot = pygame.image.load("assets/pictures/bullet_red.png")
+img_player_2_shoot = pygame.image.load("assets/pictures/bullet_green.png")
 
 clock = pygame.time.Clock()
 fail = False
@@ -127,6 +128,7 @@ while fail == False:
             if event.key == pygame.K_q:
                 k_q = False
                 player_2_speed = 6
+                player_2_shoot_list.append((player_2_pos[0], player_2_pos[1], player_2_rotation + player_2_gun_rotation, player_2_shoot_speed))
                 
             
     if k_left:
@@ -158,6 +160,10 @@ while fail == False:
     #pygame.draw.circle(screen, colors.red, player_2_pos, 50, )
     show_player(img_player_1_tank, player_1_pos, player_1_rotation)
     show_player(img_player_2_tank, player_2_pos, player_2_rotation)
+    
+    player_1_shoot_list = draw_shoot(player_1_shoot_list, img_player_1_shoot)
+    player_2_shoot_list = draw_shoot(player_2_shoot_list, img_player_2_shoot)
+    
     show_player(img_player_1_gun, player_1_pos, player_1_rotation + player_1_gun_rotation)
     show_player(img_player_2_gun, player_2_pos, player_2_rotation + player_2_gun_rotation)
     
