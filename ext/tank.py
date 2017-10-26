f0d6088df1602c323d3fcd25d219799d9b408a40
AtomTo4st <33085484+AtomTo4st@users.noreply.github.com>
2017-10-26 11:34:38 +0200

    tank class

--- ext/tank.py
+++ ext/tank.py
@@ -3,12 +3,12 @@ import sys
 sys.path.append("../")
 
 colors = ["red", "green", "blue", "white"]
+Position = namedtuple("Position", ["x","y"])
 
 class Tank:
-    def __init__(self, _id=0, posx=0, posy=0, rot=0, aim=0, speed=6, turnspeed=5):
+    def __init__(self, _id=0, pos, rot=0, aim=0, speed=6, turnspeed=5):
         self.id = _id
-        self.posx = posx
-        self.posy = posy
+        self.pos = Position(pos[0], pos[1])
         self.rot = rot
         self.aim = aim
         self.speed = speed
@@ -18,5 +18,15 @@ class Tank:
         self.body = pygame.image.load("/assets/pictures/tank_{}_bottom.png".format(colors[self.id]))
         self.turret = pygame.image.load("/assets/pictures/tank_{}_top.png".format(colors[self.id]))
     
-    def draw(self):
-        pass
+    def assignRot(self, pic, rot):
+        rotated = pygame.transform.rotate(pic, rot + 270)
+        rect = rotated.get_rect()
+        rect.center = (self.pos.x - (rect[2] / 2),self.pos.y - (rect[2] / 2))
+        return rotated, rect.center
+    
+    def draw(self, screen, pos, angle):
+        self.pos = Position(pos[0], pos[1])
+        self.rot = angle
+        screen.blit(self.assignRot(self.body, self.rot))
+        screen.blit(self.assignRot(self.turret, self.rot + self.aim))
+        return screen
