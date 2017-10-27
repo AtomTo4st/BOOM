0bd2fc10a66c99ecefa4833ba0e2ff33e559211c
discochance <bastelbach@web.de>
2017-10-27 13:46:11 +0200

    merge?

--- ext/obstacles.py
+++ ext/obstacles.py
@@ -1,71 +1,77 @@
-import pygame, numpy as np
+import pygame
 from collections import namedtuple
 from _operator import pos
-from tank import Tank
+from ext.tank import Tank
+from keyboard.keyboard import play
 
 Position = namedtuple("Position", ["x", "y"])
 
-class Obstacle(pygame.sprite.Sprite):
+
+
+class Obstacle():
     
-    def __init__(self, pos = Position(0, 0)):
-        self.pos = pos
-        sand = Sand(Position(0, 0), Position(0, 0))
-        sand2 = Sand(Position(0, 0), Position(0, 0))
-        stone = Stone(Position(0, 0), Position(0, 0))
-        stone2 = Stone(Position(0, 0), Position(0, 0))
-        magiccccFog = MagiccccFog(Position(0, 0), Position(0, 0))
-        bermuddaHole = BermuddaHole(Position(0, 0), Position(0, 0))
+    def __init__(self):
+        sand = Sand(Position(0, 891), Position(402, 1200))
+        sand2 = Sand(Position(402, 343), Position(805, 693))
+        sand3 = Sand(Position(842, 115), Position(413, 1200))
+        sand4 = Sand(Position(700, 0), Position(890, 155))
+        sand5 = Sand(Position(1325, 0), Position(1740, 525))
+        sand6 = Sand(Position(1325, 0), Position(1800, 173))
+        stone = Stone(Position(209, 160), Position(402, 343))
+        stone2 = Stone(Position(838, 541), Position(960, 656))
+        magiccccFog = MagiccccFog(Position(1320, 750), Position(1800, 1200))
+        bermuddaHole = BermuddaHole(Position(1426, 857), Position(1800, 1200))
         
-        self.listObstacles = [sand, sand2, stone, stone2, bermuddaHole, magiccccFog]
-        
+        self.listObstacles = [sand, sand2,sand3,sand4,sand5,sand6, stone, stone2, bermuddaHole, magiccccFog]
+
     def onObstacle(self, playerPos = Position(0,0)):
         for obs in self.listObstacles:
             if playerPos.x > obs.pos.x and playerPos.x < obs.pos2.x:
                 if playerPos.y > obs.pos.y and playerPos.y < obs.pos2.y:
                     return obs.type
-            return ""
+        return ""
         
-class Sand(pygame.sprite.Sprite):
+class Sand():
     
     #Why am I so slow? :/
     
     #Effect: Sand reduces the velocity of the player, for instance the tank
     
-    def __init(self, pos = Position(0,0), pos2 = Position(0,0), type = "Sand"):
+    def __init__(self, pos = Position(0,0), pos2 = Position(0,0)):
         self.pos = pos  
         self.pos2 = pos2
-        self.type = type              
+        self.type = "Sand"              
     
-class Stone(pygame.sprite.Sprite):
+class Stone():
     
     #Boom! The player has faced a massive swatter and died instantly
     
     #Effect: Player dies (Tank explodes or smt like that)
     
-    def __init(self, pos = Position(0,0), pos2 = Position(0,0), type = "Stone"):
+    def __init__(self, pos = Position(0,0), pos2 = Position(0,0)):
         self.pos = pos
         self.pos2 = pos2
-        self.type = type
+        self.type = "Stone"
     
-class MagiccccFog(pygame.sprite.Sprite):
+class MagiccccFog():
     
     #Hugh?! Where am I? What is happening to meeee o.0... Ohhh no... My steering was inverted
     
     #Effect: Invert steering
     
-    def __init(self, pos = Position(0,0), pos2 = Position(0,0), type = "MagiccccFog"):
+    def __init__(self, pos = Position(0,0), pos2 = Position(0,0)):
         self.pos = pos
         self.pos2 = pos2
-        self.type = type
+        self.type = "MagiccccFog"
     
-class BermuddaHole(pygame.sprite.Sprite):
+class BermuddaHole():
     
     #It's so dark in here... This is the end... The player has been swallowed, he disappeared and he won't return
     
     #Effect: Tank disappeares
     
-    def __init(self, pos = Position(0,0), pos2 = Position(0,0), type = "BermuddaHole"):
+    def __init__(self, pos = Position(0,0), pos2 = Position(0,0)):
         self.pos = pos
         self.pos2 = pos2
-        self.type = type
+        self.type = "BermuddaHole"
     
--- main.py
+++ main.py
@@ -144,6 +144,22 @@ class Manager:
                               BAPI.getWindow().carManager.getListOfCars()[0].position.x,
                               BAPI.getWindow().carManager.getListOfCars()[0].angle))'''
             for i in range(len(self.tank_list)):
+                if obst.onObstacle(self.car_list[i].position) == "Sand":
+                    # geschwindigkeit reduzieren
+                    self.tank_list[i].obstacle = 1
+                    print("SAAAAAND")
+                elif obst.onObstacle(self.car_list[i].position) == "Stone":
+                    #zuende
+                    self.tank_list[i].obstacle = 2
+                elif obst.onObstacle(self.car_list[i].position) == "MagiccccFog":
+                    #invertiert lenkung
+                    self.tank_list[i].obstacle = 3
+                elif obst.onObstacle(self.car_list[i].position) == "BermuddaHole":
+                    #unsichtbar, kann nicht schiessen
+                    self.tank_list[i].obstacle = 4
+                else:
+                     self.tank_list[i].obstacle = 0
+                
                 self.steerTanks(i,self.remote.get_in_throttle(self.remote_ip + str(i)),self.remote.get_in_steer(self.remote_ip + str(i)))
                 #self.steerTanks_debug(i)
                 self.screen, self.hit = self.tank_list[i].draw(self.screen,self.car_list[i].position, -self.car_list[i].angleInDegree + 90, self.steer,  self.car_list[(i+1)%2].position)
@@ -168,7 +184,12 @@ class Manager:
                 self.car_list[id].steeringAngle = 0
             else:
                 self.tank_list[id].trigger = False 
-                self.car_list[id].throttle = self.maxThrottle_live 
+                if  self.tank_list[id].obstacle == 1:
+                    self.car_list[id].throttle = self.maxThrottle_live - 5
+                elif  self.tank_list[id].obstacle == 2:
+                    self.car_list[id].throttle = 0
+                else:
+                    self.car_list[id].throttle = self.maxThrottle_live 
                 self.car_list[id].steeringAngle = steering - self.offset
             self.steer = steering - self.offset
         else:
@@ -176,39 +197,24 @@ class Manager:
             self.car_list[id].throttle = 0
             self.car_list[id].steeringAngle = 0
 
+        if self.tank_list[id].obstacle == 3:
+            self.car_list[id].steeringAngle*=-1
         steer_override = limitToUInt8(self.car_list[id].steeringAngle + self.offset)
         throttle_override = limitToUInt8(self.car_list[id].throttle + self.offset)
-        print( throttle_override, ";", steer_override)
         self.remote.set_override_out_both(self.remote_ip+str(id), steer_override, throttle_override)
-            
+                    
     def steerTanks_debug(self, id):
-        if not self.hit:
-            if keyboard.is_pressed("f"):
-                self.car_list[id].throttle = self.minThrottle
-                self.car_list[id].steeringAngle = 0
-                self.tank_list[id].trigger = True
-                if keyboard.is_pressed("a"):
-                    self.steer = -100
-                elif keyboard.is_pressed("d"):
-                    self.steer = 100
-                else:
-                    self.steer=0
-            else:
-                self.tank_list[id].trigger = False 
-                self.car_list[id].throttle = self.maxThrottle 
-                if keyboard.is_pressed("a"):
-                    self.car_list[1].steeringAngle = -100
-                    self.steer = -100
-                elif keyboard.is_pressed("d"):
-                    self.car_list[1].steeringAngle = 100
-                    self.steer = 100
-                else:
-                    self.car_list[1].steeringAngle = 0
-                    self.steer = 0
+        if keyboard.is_pressed("f"):
+            throttle=201
+        else :
+            throttle = 0
+        if keyboard.is_pressed("a"):
+            steer = 28
+        elif keyboard.is_pressed("d"):
+            steer = 228
         else:
-            self.car_list[id].throttle = 0
-            self.car_list[id].steeringAngle = 0
-            self.steer = 0
+            steer = 128
+        self.steerTanks(id, throttle, steer)
             
 
 def initMainWindow(name, fieldWidthPx, fieldHeightPx):
